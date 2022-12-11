2a5c844fa8299162f4bbf76ad21cee6c5b3fc723
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-12-11 13:44:12 +0600

    added design

--- src/Pages/Home/AboutMe/AboutMe.js
+++ src/Pages/Home/AboutMe/AboutMe.js
@@ -16,7 +16,7 @@ const AboutMe = () => {
           <div className="flex flex-col items-center text-center justify-center">
             <h2 className="font-medium text-2xl title-font mt-4 text-info">Md: Imtius Ahammed</h2>
             <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-            <p className="text-base"> Dedicated and efficient full stack developer with 6+ years experience in application layers, presentation layers, and databases. Certified in both F/E and B/E technologies. Spearheaded successful transition from LAMP stack to MEAN which cut latency by 40% and increased effectiveness of database administrators by 20%. Seeking to further improve HTML5 and CSS3 skills as the future full stack developer at Atmospheric Solutions.</p>
+            <p className="text-base"> Dedicated and efficient full stack developer with 1+ years experience in application layers, presentation layers, and databases.I am very much familiar with HTML5,Bootstrap,Tailwind,JavaScript,React,  Seeking to further improve React and Node.js skills as the future full stack developer at Atmospheric Solutions.</p>
           </div>
         </div>
         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
@@ -24,16 +24,12 @@ const AboutMe = () => {
           <p className="leading-relaxed text-lg mb-4">  Web development is the building and maintenance of websites;
                   it’s the work that happens behind the scenes to make a website
                   look great, work fast and perform well with a seamless user
-                  experience. Web developers, or ‘devs’, do this by using a
-                  variety of coding languages. The languages they use depends on
-                  the types of tasks they are preforming and the platforms on
-                  which they are working. Web development skills are in high
-                  demand worldwide and well paid too – making development a
-                  great career option. It is one of the easiest accessible
-                  higher paid fields as you do not need a traditional university
-                  degree to become qualified. The field of web development is
-                  generally broken down into front-end (the user-facing side)
-                  and back-end (the server side). Let’s delve into the details.
+                  experience. I do this by using a
+                  variety of coding languages. The languages i use depends on
+                  the types of tasks i  preforming and the platforms on
+                  which i am working. Web development skills are in high
+                  demand worldwide  making development a
+                  great career option. I want to explore more about this field and i like to explore new technologies.
           <img
                 alt="content" 
                 className="lg:w-full w-10/12 mx-auto mt-4 rounded-lg overflow-hidden "
--- src/Pages/Home/ProjectSS/ProjectSS.js
+++ src/Pages/Home/ProjectSS/ProjectSS.js
@@ -77,7 +77,7 @@ const ProjectSS = () => {
               <div className="flex items-center justify-center">
                 
                 <a href=""><button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  CodeLink
+                  Details
                 </button></a>
                <a href={liveSite} target='_blank'> <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                  LiveSite
